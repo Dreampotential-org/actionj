2bfcef742fabf9c4f84c76e1028e49817449060b
Aaron Rosen <aaronorosen@gmail.com>
2020-11-02 23:46:46 -0800

    populate meds ui

--- www/js/meds.js
+++ www/js/meds.js
@@ -3,6 +3,59 @@ function init_meds() {
     $("body").delegate("#add_meds", "click", function(e) {
         add_meds()
     })
+
+    $("body").delegate(".med-delete", "click", function(e) {
+        delete_med($(this).attr("med_id"))
+    })
+
+    list_meds(function(meds) {
+        for(var med of meds) {
+            add_med_html(med)
+        }
+    })
+}
+
+function add_med_html(med) {
+    $("#med_list").append(`
+        <tr>
+            <td>${med.name}</td>
+            <td>${med.dosage}</td>
+            <td>
+                <a href='#' class='med-delete' med_id=${med.id}>
+                    <span class="glyphicon glyphicon-trash"
+                        style="font-size: 19px;color: white;"></span>
+                </a>
+            </td>
+        </tr>
+    `)
+}
+
+
+function delete_med(med_id) {
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": SERVER + "sfapp2/api/del_med/" + med_id,
+        "method": "DELETE",
+        "processData": false,
+        "contentType": false,
+        "mimeType": "multipart/form-data",
+        "headers": {
+            "Authorization": localStorage.getItem("token"),
+        },
+
+    }
+    $.ajax(settings).done(function (response) {
+        // change screen for code collecton
+        swal({
+          title: "Good job!",
+          text: "Medication Removed",
+          icon: "success",
+        });
+
+    }).fail(function (err) {
+      alert("ERROR")
+    });
 }
 
 function add_meds() {
@@ -53,7 +106,7 @@ function list_meds(callback) {
     }
     $.ajax(settings).done(function (response) {
         // change screen for code collecton
-        callback(JSON.parse(response))
+        callback(JSON.parse(response).meds)
     }).fail(function (err) {
       alert("ERROR")
     });
