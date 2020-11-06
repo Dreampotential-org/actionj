e7b98a0db2b8b828978cee0529947779e275970c
Aaron Rosen <aaronorosen@gmail.com>
2020-11-05 22:53:58 -0800

    typo

--- www/js/segments.js
+++ www/js/segments.js
@@ -1,8 +1,8 @@
 var segments = [
-    {'type': 'speed_read': 'textarea_div': 'display this text on page in div'},
-    {'type': 'title_text': 'title': 'Here is a title in bold on page',
+    {'type': 'speed_read', 'textarea_div': 'display this text on page in div'},
+    {'type': 'title_text', 'title': 'Here is a title in bold on page',
       'textarea_div': 'display this text on page in div'},
-    {'type': 'question_choices': 'title': 'Here is a title in bold on page',
+    {'type': 'question_choices', 'title': 'Here is a title in bold on page',
       'choices': ['choice string 1 via radio button', 'two string'],
       'textarea_div': 'display this text on page in div'},
 ]
