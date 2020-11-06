7e65ca7500aab2926a2c93383e0f46c1fd4d7f1b
Aaron Rosen <aaronorosen@gmail.com>
2020-11-05 22:50:22 -0800

    add test segments file

---
+++ www/js/segments.js
@@ -0,0 +1,11 @@
+var segments = [
+    {'type': 'speed_read': 'textarea_div': 'display this text on page in div'},
+    {'type': 'title_text': 'title': 'Here is a title in bold on page',
+      'textarea_div': 'display this text on page in div'},
+    {'type': 'question_choices': 'title': 'Here is a title in bold on page',
+      'choices': ['choice string 1 via radio button', 'two string'],
+      'textarea_div': 'display this text on page in div'},
+]
+
+function display_segment(n) {
+}
