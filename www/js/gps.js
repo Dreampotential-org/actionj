23dab6d46d8dbfb73f6158699de52e7c7d258e91
Aaron Rosen <aaronorosen@gmail.com>
2020-11-10 13:59:34 -0800

    integrate gps

--- www/js/gps.js
+++ www/js/gps.js
@@ -1,3 +1,6 @@
+var CURRENT_POSITION = null;
+var CURRENT_POSITION_LOW = null;
+
 function setup_gps_events() {
     $("body").delegate("#post_gps", "click", function(e) {
         gps_checkin()
@@ -9,6 +12,8 @@ function handle_gps_click() {
     if (localStorage.getItem("token")) {
         hide_screens()
         $("#map-check").show()
+        start_gps();
+        found_gps_or_timeout();
     }
     else {
         swal({
@@ -20,11 +25,47 @@ function handle_gps_click() {
     }
 }
 
+function found_gps_or_timeout() {
+    $('#LocationModal').removeClass('is-visible');
+    swal({
+        title: "Checking for GPS Signal",
+        text: "Please wait while we find GSP location",
+        icon: "info",
+        buttons: false,
+        closeOnEsc: false,
+        closeOnClickOutside: false,
+    });
+
+    setTimeout(function () {
+        var counter = 0;
+        var i = setInterval(function () {
+            if (CURRENT_POSITION == null && CURRENT_POSITION_LOW == null) {
+                console.log("No GPS Signal. Try again");
+            } else {
+                // XXX These values are not getting correctly set on android.
+                swal({
+                    title: "GPS Location Found",
+                    text: "Now, enter event and submit",
+                    icon: "success",
+                });
+
+                clearInterval(i);
+            }
+            counter++;
+        }, 200);
+    }, 20);
+}
+
 function gps_checkin() {
     var form = new FormData();
     form.append("msg", $("#map-check textarea").val());
-    form.append("lat", '');
-    form.append("lng", '');
+    if (CURRENT_POSITION != null) {
+        form.append("lat", CURRENT_POSITION.coords.latitude);
+        form.append("lng", CURRENT_POSITION.coords.longitude);
+    } else {
+        form.append("lat", CURRENT_POSITION_LOW.coords.latitude);
+        form.append("lng", CURRENT_POSITION_LOW.coords.longitude);
+    }
 
     var settings = {
         "async": true,
@@ -102,4 +143,75 @@ function get_journal_list(callback) {
     }).fail(function (err) {
       alert("ERROR")
     });
+}
+
+
+function start_gps() {
+    var geo_options_low = {
+        enableHighAccuracy: false,
+        maximumAge: 30000,
+        timeout: 27000
+    };
+
+    if (isApp()) {
+        document.addEventListener('deviceready', function () {
+            navigator.geolocation.watchPosition(
+                geo_success_low, geo_error, geo_options_low
+            );
+        }, false);
+    } else {
+        navigator.geolocation.watchPosition(
+            geo_success_low, geo_error, geo_options_low
+        );
+    }
+
+    function geo_error(err) {
+
+        if (err.code == 1 || err.code == err.PERMISSION_DENIED ||
+            err.code == err.UNKNOWN_ERROR) {
+            swal({
+                title: "GPS Issue.",
+                text: "Please allow gps permission",
+                icon: "error",
+            });
+        }
+        console.log("errror no gps")
+        console.warn('ERROR(' + err.code + '): ' + err.message);
+        start_gps()
+    }
+
+    geo_options = {
+        enableHighAccuracy: true,
+        maximumAge: 30000,
+        timeout: 27000
+    };
+
+    if (isApp()) {
+        document.addEventListener('deviceready', function () {
+            navigator.geolocation.watchPosition(
+                geo_success, geo_error, geo_options
+            );
+        }, false);
+    }else{
+        navigator.geolocation.watchPosition(
+            geo_success, geo_error, geo_options
+        );
+    }
+
+    function geo_success_low(position) {
+        CURRENT_POSITION_LOW = position
+        console.log(position.coords.latitude +
+            " " + position.coords.longitude);
+    }
+
+    function geo_success(position) {
+        CURRENT_POSITION = position
+        console.log(position.coords.latitude +
+            " " + position.coords.longitude);
+    }
+}
+
+function isApp() {
+    return (typeof(cordova) !== 'undefined' ||
+            typeof(phonegap) !== 'undefined');
 }
