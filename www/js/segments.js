e1bfd98d9636385a32f1e6634321b6b335232ff8
Aaron Rosen <aaronorosen@gmail.com>
2020-11-11 16:04:30 -0800

    Extract changes from santosh-dev-1111

--- www/js/segments.js
+++ www/js/segments.js
@@ -1,48 +1,81 @@
 var segments = [
-    {'id': 1, 'type': 'speed_read',
-     'textarea_div': 'display this text on page in div'},
-    {'id': 2, 'type': 'title_text', 'title': 'Here is a title in bold on page',
-     'textarea_div': 'display this text on page in div'},
-    {'id': 3, 'type': 'question_choices',
-     'title': 'Here is a title in bold on page',
-     'choices': ['choice string 1 via radio button', 'two string'],
-     'textarea_div': 'display this text on page in div'},
+  {
+    id: 1,
+    type: 'speed_read',
+    textarea_div: 'display this text on page in div'
+  },
+  {
+    id: 2,
+    type: 'title_text',
+    title: 'Here is a title in bold on page',
+    textarea_div: 'display this text on page in div'
+  },
+  {
+    id: 3,
+    type: 'question_choices',
+    title: 'Here is a title in bold on page',
+    choices: ['choice string 1 via radio button', 'two string'],
+    textarea_div: 'display this text on page in div'
+  }
 ]
 
-
-function display_segment(segment) {
-    if (segment['type'] == 'speed_read') {
-        document.getElementById('textarea_div').innerHTML = 'display this text on page in div';
-    }
-    if (segment['type'] == 'title_text') {
-        document.getElementById('title').innerHTML = 'display this text on page in div title';
-        document.getElementById('textarea_div').innerHTML = 'display this text on page in div textarea';
-    }
-    if (segment['type'] == 'question_choices') {
-        document.getElementById('title').innerHTML = 'display this text on page in div choice';
-        document.getElementById('textarea_div').innerHTML = 'display this text on page in div choice';
-
-        display_question(segment['choices'])
-    }
+function display_segment (segment) {
+  // if (segment['type'] == 'speed_read') {
+  //     document.getElementById('textarea_div').innerHTML = 'display this text on page in div';
+  // }
+  // if (segment['type'] == 'title_text') {
+  //     document.getElementById('title').innerHTML = 'display this text on page in div title';
+  //     document.getElementById('textarea_div').innerHTML = 'display this text on page in div textarea';
+  // }
+  // if (segment['type'] == 'question_choices') {
+  //     document.getElementById('title').innerHTML = 'display this text on page in div choice';
+  //     document.getElementById('textarea_div').innerHTML = 'display this text on page in div choice';
+  //     display_question(segment['choices'])
+  // }
 }
 var currentTab = 0
 
-function init() {
-    display_segment(segments[currentTab])
+function init () {
+  // display_segment(segments[currentTab])
+  // display_question(segments[currentTab])
+
+  var html = ''
+  var que = ''
+
+  segments.forEach((element, i) => {
+    que += `<div class="tab" question_id=${element.id} id="tab${i}">
+                      ${getOptions(i)}
+                    </div>`
+    html += `<span class="step"></span>`
+  })
+
+  que += `<div style="overflow:auto;">
+                <div style="float:right;">
+                    <button type="button" id="prevBtn"
+                        onclick="nextPrev(-1)">Previous</button>
+                    <button type="button" id="nextBtn"
+                        onclick="nextPrev(1)">Next</button>
+                </div>
+            </div>`
+
+  que += `<div id="custom-indicator"
+            style="text-align:center;margin-top:40px;">${html}</div>`
+
+  $('#segForm').append(que)
+  showTab(currentTab)
 }
 
-function display_question(segment) {
-    var html = ''
-    var que = ''
-
-    console.log(questions)
-    que += `<div class="tab" segment_id=${segment.id} id="tab${i}">
+function display_question (segment) {
+  var html = ''
+  var que = ''
+  // console.log(questions)
+  que += `<div class="tab" segment_id=${segment.id} id="tab${currentTab}">
                     <h4>${segment.title}</h4>
-                    ${getOptions(segment.choices, i)}
+                    ${getOptions(segment.choices, currentTab)}
                 </div>`
-    html += `<span class="step"></span>`
+  html += `<span class="step"></span>`
 
-    que += `<div style="overflow:auto;">
+  que += `<div style="overflow:auto;">
                 <div style="float:right;">
                     <button type="button" id="prevBtn"
                         onclick="nextPrev(-1)">Previous</button>
@@ -51,26 +84,60 @@ function display_question(segment) {
                 </div>
             </div>`
 
-    que += `<div id="custom-indicator"
+  que += `<div id="custom-indicator"
             style="text-align:center;margin-top:40px;">${html}</div>`
 
-    $('#regForm').append(que)
-    showTab(currentTab)
+  $('#segForm').append(que)
+  showTab(currentTab)
 }
 
 var answers = []
-function getOptions(choices, i) {
-    var opt = ''
-    choices.forEach((item, ind) => {
-        opt += `
-            <div class="radio">
-                <label><input type="radio" value="${item.id}"
-                        oninput="this.className = ''" name="que${i}">
-                            ${item.choice_text}</label>
-            </div>`
-    })
+function getOptions (i) {
+  // var opt = ''
+  // if (choices != undefined) {
+  //   choices.forEach((item, ind) => {
+  //     opt += `
+  //                   <div class="radio">
+  //                       <label><input type="radio" value="${item.id}"
+  //                               oninput="this.className = ''" name="que${i}">
+  //                                   ${item.choice_text}</label>
+  //                   </div>`
+  //   })
+  // }
 
-    return opt
+  // return opt
+
+  var view = ''
+
+  if (segments[i].type == 'speed_read') {
+    view = `<div class="form-group">
+                <textarea class="form-control" id="speed_read_input" rows="7" placeholder="">${
+  segments[i].textarea_div
+}</textarea>
+            </div>`
+  } else if (segments[i].type == 'title_text') {
+    view = `<div class="form-group">
+                <input type="text" class="form-control" id="playtitle"  placeholder="Title" value="${
+  segments[i].title
+}" readonly>
+            </div>
+            <div class="form-group">
+                <textarea class="form-control" id="playspeed_read_input" rows="7" placeholder="" value="">${
+  segments[i].textarea_div
+}</textarea>
+            </div>`
+  } else {
+    view = `<h4>${segments[i].title}</h4>`
+    segments[i].choices.forEach(item => {
+      view += `<div class="radio">
+                        <label><input type="radio" value="${item}"
+                                oninput="this.className = ''" name="que${i}">
+                                    ${item}</label>
+                    </div>`
+    })
+  }
+
+  return view
 }
 
 // var currentTab = 0; // Current tab is set to be the first tab (0)
@@ -78,67 +145,67 @@ function getOptions(choices, i) {
 
 var isDisable = true
 
-function showTab(n) {
-    // This function will display the specified tab of the form...
+function showTab (n) {
+  // This function will display the specified tab of the form...
 
-    $('input[type=radio]').change(function () {
-        isDisable = false
-        $('#nextBtn').removeAttr('disabled')
-    })
+  $('input[type=radio]').change(function () {
+    isDisable = false
+    // $('#nextBtn').removeAttr('disabled')
+  })
 
-    $('#prevBtn').click(function () {
-        isDisable = false
-        $('#nextBtn').removeAttr('disabled')
-    })
+  $('#prevBtn').click(function () {
+    isDisable = false
+    // $('#nextBtn').removeAttr('disabled')
+  })
 
-    if (isDisable)
-        $('#nextBtn').attr('disabled', true)
+  // if (isDisable) $('#nextBtn').attr('disabled', true)
 
-    if (n >= questions.length) {
-
-        alert("HEREEE")
-        set_user_answers()
-        // send results to backend
-        return
-    }
-    var x = document.getElementsByClassName('tab')
-    x[n].style.display = 'block'
-    // ... and fix the Previous/Next buttons:
-    if (n == 0) {
-        document.getElementById('prevBtn').style.display = 'none'
-    } else {
-        document.getElementById('prevBtn').style.display = 'inline'
-    }
-    if (n == x.length - 1) {
-        document.getElementById('nextBtn').innerHTML = 'Submit'
-    } else {
-        document.getElementById('nextBtn').innerHTML = 'Next'
-    }
-    // ... and run a function that will display the correct step indicator:
-    fixStepIndicator(n)
+  if (n >= segments.length) {
+    alert('HEREEE')
+    set_user_answers()
+    // send results to backend
+    return
+  }
+  var x = document.getElementsByClassName('tab')
+  x[n].style.display = 'block'
+  // ... and fix the Previous/Next buttons:
+  if (n == 0) {
+    document.getElementById('prevBtn').style.display = 'none'
+  } else {
+    document.getElementById('prevBtn').style.display = 'inline'
+  }
+  if (n == x.length - 1) {
+    // document.getElementById('nextBtn').innerHTML = 'Submit'
+    document.getElementById('nextBtn').style.display = 'none'
+  } else {
+    document.getElementById('nextBtn').style.display = 'inline'
+    document.getElementById('nextBtn').innerHTML = 'Next'
+  }
+  // ... and run a function that will display the correct step indicator:
+  fixStepIndicator(n)
 }
 
-function nextPrev(n) {
-    // populate  question_answers
-    question_answers[$("#tab" + currentTab).attr("question_id")] = (
-            $("#tab" + currentTab + " .radio input:checked").val())
+function nextPrev (n) {
+  // populate  question_answers
+  // question_answers[$('#tab' + currentTab).attr('question_id')] = $(
+  //   '#tab' + currentTab + ' .radio input:checked'
+  // ).val()
 
-    // This function will figure out which tab to display
-    var x = document.getElementsByClassName('tab')
-    // Exit the function if any field in the current tab is invalid:
-    if (n == 1 && !validateForm())
-        return false
-    // Hide the current tab:
-    x[currentTab].style.display = 'none'
-    // Increase or decrease the current tab by 1:
-    currentTab = currentTab + n
-    // if you have reached the end of the form...
-    if (currentTab >= x.length) {
-        // ... the form gets submitted:
-        // return false;
-    }
-    // Otherwise, display the correct tab:
-    showTab(currentTab)
+  // This function will figure out which tab to display
+  var x = document.getElementsByClassName('tab')
+  // Exit the function if any field in the current tab is invalid:
+  // if (n == 1 && !validateForm()) return false
+  // Hide the current tab:
+  x[currentTab].style.display = 'none'
+  // Increase or decrease the current tab by 1:
+  currentTab = currentTab + n
+  // if you have reached the end of the form...
+  if (currentTab >= x.length) {
+    // ... the form gets submitted:
+    // return false;
+  }
+  // Otherwise, display the correct tab:
+  showTab(currentTab)
 }
 
 function fixStepIndicator (n) {
@@ -152,6 +219,5 @@ function fixStepIndicator (n) {
   // ... and adds the "active" class on the current step:
   x[n].className += ' active'
 }
-
 
 window.addEventListener('DOMContentLoaded', init, false)
