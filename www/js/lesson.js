21fb8a7691016fc9b7d5c025a342e79ca8741f59
swornim00 <swornimshrestha@Swornims-MacBook-Pro.local>
2020-12-18 11:15:44 +0545

    Add and Edit Merged

--- www/js/lesson.js
+++ www/js/lesson.js
@@ -3,11 +3,178 @@ var title_text_count = 0;
 var question_choices_count = 0;
 var video_file_count = 0;
 var iframe_link_count = 0
+var MODE;
 //var API_SERVER = "http://localhost:8000";
 
 var API_SERVER ='https://sfapp-api.dreamstate-4-all.org';
 var SERVER = 'https://sfapp.dreamstate-4-all.org';
-$("#add_form").submit((e) => {
+var lesson_id=  getParam("lesson_id");
+
+function addChoices(id,value){
+    if(!value){
+        value = ""
+    }
+    var next_id = $("#choices_"+id).children().last().data("id") + 1
+    //console.log(id)
+    $("#choices_"+id).append('<div><input type="text" class="form-control" data-id="'+next_id+'"rows="7" placeholder="Choices" value="'+value+'"><button onclick="$(this).parent().remove()">Remove Choice</button></div>')
+}
+
+function getParam(sParam){
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++){
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) {
+            return sParameterName[1];
+        }
+    }
+}
+
+function addSpeedRead(isNew,id, value) {
+    //console.log(quick_read_count)
+        if(!isNew){
+            $("#speed_read").find("textarea").html(value)
+            $("#speed_read").find("textarea").attr("data-id",id)
+        }else{
+            $("#speed_read").find("textarea").html("")
+        }
+        $("#speed_read").find("textarea").attr("name","speed_read_"+quick_read_count)
+        $("#sortable").append($("#speed_read").html())
+        quick_read_count++;
+    }
+
+function addTitleText(isNew,id,title,text){
+    if(!isNew){
+        $("#title_text").find("input[type=text]").attr("value",title)
+        $("#title_text").find("textarea").html(text)
+        $("#title_text").find("input[type=text]").attr("data-id",id)
+        $("#title_text").find("textarea").attr("data-id",id)
+
+    }else{
+        $("#title_text").find("input[type=text]").attr("value","")
+        $("#title_text").find("textarea").html("")
+    }
+    $("#title_text").find("textarea").attr("name","text_"+title_text_count)
+    $("#title_text").find("input[type=text]").attr("name","title_"+title_text_count)
+
+    $("#sortable").append($("#title_text").html())
+    title_text_count++;
+}
+
+function addQuestionChoices(isNew,id,question,choices,image){
+
+    $("#question_choices").find("input").first().attr("name","question_"+question_choices_count)
+    $("#question_choices").find("#choices").attr("id","choices_"+question_choices_count)
+    $("#question_choices").find("input").last().attr("name","image_"+question_choices_count)
+    $("#question_choices").find("button").attr("onclick","addChoices("+question_choices_count+")");
+
+    if(!isNew){
+        $("#question_choices").find("input").first().attr("value",question)
+        $("#question_choices").find("input").last().attr("value",image)
+
+        $("#question_choices").find("input").first().attr("data-id",id)
+        $("#question_choices").find("input").last().attr("data-id",id)
+        $("#choices_"+question_choices_count).find('input').remove();
+        choices.split(",").forEach(function(choice){
+            //console.log(choice)
+            addChoices(question_choices_count,choice)
+        })
+
+    }else{
+        $("#question_choices").find("input").first().attr("value","")
+        $("#question_choices").find("text").html("")
+        $("#question_choices").find("input").last().attr("value","")
+    }
+
+
+
+    $("#sortable").append($("#question_choices").html())
+    question_choices_count++;
+}
+
+function addVideoFile(isNew,id,question,choices,image){
+
+    if(!isNew){
+        $("#video_file").find("input").first().attr("value",question)
+        $("#video_file").find("input").last().attr("value",image)
+
+        $("#video_file").find("input").first().attr("data-id",id)
+        $("#video_file").find("input").last().attr("data-id",id)
+        
+    }else{
+        $("#video_file").find("input").first().attr("value","")
+        $("#video_file").find("input").last().attr("value","")
+
+    }
+
+    $("#video_file").find("input").first().attr("name","question_"+video_file_count)
+    $("#video_file").find("input").last().attr("name","video_"+video_file_count)
+
+    $("#sortable").append($("#video_file").html())
+    video_file_count++;
+    }
+
+
+function addIframeLink(isNew,id,question,choices,image){
+
+    if(!isNew){
+        $("#iframe_link").find("input").first().attr("value",question)
+        $("#iframe_link").find("input").last().attr("value",image)
+
+        $("#iframe_link").find("input").first().attr("data-id",id)
+        $("#iframe_link").find("input").last().attr("data-id",id)
+        
+    }else{
+        $("#iframe_link").find("input").first().attr("value","")
+        $("#iframe_link").find("input").last().attr("value","")
+
+    }
+
+    $("#iframe_link").find("input").first().attr("name","question_"+iframe_link_count)
+    $("#iframe_link").find("input").last().attr("name","link_"+iframe_link_count)
+
+    $("#sortable").append($("#iframe_link").html())
+    iframe_link_count++;
+    }
+
+if(lesson_id){
+    MODE = "UPDATE";
+}else{
+    MODE = "CREATE";
+}
+
+if(MODE =="UPDATE"){
+    $.get(API_SERVER+'/courses_api/lesson/read/'+lesson_id+'/',function(response) {
+        $("#lesson_slide").attr("href",SERVER+"/slide.html?lesson_id="+lesson_id)
+        var flashcards = response.flashcards;
+        flashcards.forEach((flashcard) => {
+            console.log(flashcard)
+
+            if(flashcard.lesson_type == "quick_read"){
+                
+                addSpeedRead(false,flashcard.id,flashcard.question)
+            }
+            if(flashcard.lesson_type == "title_text"){
+                addTitleText(false,flashcard.id,flashcard.question,flashcard.answer)
+            }
+            if(flashcard.lesson_type == "question_choices"){
+                addQuestionChoices(false,flashcard.id,flashcard.question,flashcard.options,flashcard.image)
+            }
+
+            if(flashcard.lesson_type == "video_file"){
+                addVideoFile(false,flashcard.id,flashcard.question,flashcard.options,flashcard.image)
+            }
+
+            if(flashcard.lesson_type == "iframe_link"){
+                addIframeLink(false,flashcard.id,flashcard.question,flashcard.options,flashcard.image)
+            }
+
+        })
+        
+    })
+}
+
+$("#lesson_form").submit((e) => {
     e.preventDefault()
     data_ = {
     "lesson_name":"test"
@@ -95,195 +262,45 @@ $("#add_form").submit((e) => {
         flashcards.push(temp)
         number++;
     }
+
     data_.flashcards = flashcards
     console.log(data_)
     
-    $.ajax({
-        "url": API_SERVER +"/courses_api/lesson/create",
-        'data': JSON.stringify(data_),
-        'type': 'POST',
-        'contentType': 'application/json',
-        'success': function (data){
-            console.log(data.id)
-            var currentPathName = window.location.pathname;
-            var currentPathNameArray = currentPathName.split("/");
-            var currentPathNamePop =currentPathNameArray.pop()
-            var currentPathNamePush = currentPathNameArray.push("edit.html")
-            var nextPathName =currentPathNameArray.join("/")
-            window.location.replace(nextPathName+"?lesson_id="+data.id)    
-        alert("FlashCard Created!")
+    if(MODE=="CREATE"){
+        $.ajax({
+            "url": API_SERVER +"/courses_api/lesson/create",
+            'data': JSON.stringify(data_),
+            'type': 'POST',
+            'contentType': 'application/json',
+            'success': function (data){
+                //console.log(data.id)
+                var currentPathName = window.location.pathname;
+                window.location.replace(currentPathName+"?lesson_id="+data.id)    
+            alert("FlashCard Created!")
+        }
+        })
+
+    }else{
+
+        $.ajax({
+            "url": API_SERVER+"/courses_api/lesson/update/"+lesson_id+"/",
+            'data': JSON.stringify(data_),
+            'type': 'POST',
+            'contentType': 'application/json',
+            'success': function (data){
+            alert("FlashCard Updated!")
+            location.reload()
+        }
+        })
     }
-    })
 })
 
-function addChoices(id,value){
-    if(!value){
-        value = ""
-    }
-    var next_id = $("#choices_"+id).children().last().data("id") + 1
-    console.log(id)
-    $("#choices_"+id).append('<div><input type="text" class="form-control" data-id="'+next_id+'"rows="7" placeholder="Choices" value="'+value+'"><button onclick="$(this).parent().remove()">Remove Choice</button></div>')
-}
-
-function getParam(sParam){
-    var sPageURL = window.location.search.substring(1);
-    var sURLVariables = sPageURL.split('&');
-    for (var i = 0; i < sURLVariables.length; i++){
-        var sParameterName = sURLVariables[i].split('=');
-        if (sParameterName[0] == sParam) {
-            return sParameterName[1];
-        }
-    }
-}
-
-function addSpeedRead(isNew,id, value) {
-    console.log(quick_read_count)
-        if(!isNew){
-            $("#speed_read").find("textarea").html(value)
-            $("#speed_read").find("textarea").attr("data-id",id)
-        }else{
-            $("#speed_read").find("textarea").html("")
-        }
-        $("#speed_read").find("textarea").attr("name","speed_read_"+quick_read_count)
-        $("#sortable").append($("#speed_read").html())
-        quick_read_count++;
-    }
-
-function addTitleText(isNew,id,title,text){
-    if(!isNew){
-        $("#title_text").find("input[type=text]").attr("value",title)
-        $("#title_text").find("textarea").html(text)
-        $("#title_text").find("input[type=text]").attr("data-id",id)
-        $("#title_text").find("textarea").attr("data-id",id)
-
-    }else{
-        $("#title_text").find("input[type=text]").attr("value","")
-        $("#title_text").find("textarea").html("")
-    }
-    $("#title_text").find("textarea").attr("name","text_"+title_text_count)
-    $("#title_text").find("input[type=text]").attr("name","title_"+title_text_count)
-
-    $("#sortable").append($("#title_text").html())
-    title_text_count++;
-}
-
-function addQuestionChoices(isNew,id,question,choices,image){
-
-    $("#question_choices").find("input").first().attr("name","question_"+question_choices_count)
-    $("#question_choices").find("#choices").attr("id","choices_"+question_choices_count)
-    $("#question_choices").find("input").last().attr("name","image_"+question_choices_count)
-    $("#question_choices").find("button").attr("onclick","addChoices("+question_choices_count+")");
-
-    if(!isNew){
-        $("#question_choices").find("input").first().attr("value",question)
-        $("#question_choices").find("input").last().attr("value",image)
-
-        $("#question_choices").find("input").first().attr("data-id",id)
-        $("#question_choices").find("input").last().attr("data-id",id)
-        $("#choices_"+question_choices_count).find('input').remove();
-        choices.split(",").forEach(function(choice){
-            console.log(choice)
-            addChoices(question_choices_count,choice)
-        })
-
-    }else{
-        $("#question_choices").find("input").first().attr("value","")
-        $("#question_choices").find("text").html("")
-        $("#question_choices").find("input").last().attr("value","")
-    }
-
-
-
-    $("#sortable").append($("#question_choices").html())
-    question_choices_count++;
-}
-
-function addVideoFile(isNew,id,question,choices,image){
-
-    if(!isNew){
-        $("#video_file").find("input").first().attr("value",question)
-        $("#video_file").find("input").last().attr("value",image)
-
-        $("#video_file").find("input").first().attr("data-id",id)
-        $("#video_file").find("input").last().attr("data-id",id)
-        
-    }else{
-        $("#video_file").find("input").first().attr("value","")
-        $("#video_file").find("input").last().attr("value","")
-
-    }
-
-    $("#video_file").find("input").first().attr("name","question_"+video_file_count)
-    $("#video_file").find("input").last().attr("name","video_"+video_file_count)
-
-    $("#sortable").append($("#video_file").html())
-    video_file_count++;
-    }
-
-
-function addIframeLink(isNew,id,question,choices,image){
-
-    if(!isNew){
-        $("#iframe_link").find("input").first().attr("value",question)
-        $("#iframe_link").find("input").last().attr("value",image)
-
-        $("#iframe_link").find("input").first().attr("data-id",id)
-        $("#iframe_link").find("input").last().attr("data-id",id)
-        
-    }else{
-        $("#iframe_link").find("input").first().attr("value","")
-        $("#iframe_link").find("input").last().attr("value","")
-
-    }
-
-    $("#iframe_link").find("input").first().attr("name","question_"+iframe_link_count)
-    $("#iframe_link").find("input").last().attr("name","link_"+iframe_link_count)
-
-    $("#sortable").append($("#iframe_link").html())
-    iframe_link_count++;
-    }
-
-    var lesson_id=  getParam("lesson_id");
-    if(!lesson_id){
-        alert("No Lesson Selected\nRedirecting to add.html. Please Select a lesson to open this page")
-        
-    }
-    if(lesson_id){
-        $.get(API_SERVER+'/courses_api/lesson/read/'+lesson_id+'/',function(response) {
-            $("#lesson_slide").attr("href",SERVER+"/slide.html?lesson_id="+lesson_id)
-            var flashcards = response.flashcards;
-            flashcards.forEach((flashcard) => {
-                console.log(flashcard)
-    
-                if(flashcard.lesson_type == "quick_read"){
-                    
-                    addSpeedRead(false,flashcard.id,flashcard.question)
-                }
-                if(flashcard.lesson_type == "title_text"){
-                    addTitleText(false,flashcard.id,flashcard.question,flashcard.answer)
-                }
-                if(flashcard.lesson_type == "question_choices"){
-                    addQuestionChoices(false,flashcard.id,flashcard.question,flashcard.options,flashcard.image)
-                }
-    
-                if(flashcard.lesson_type == "video_file"){
-                    addVideoFile(false,flashcard.id,flashcard.question,flashcard.options,flashcard.image)
-                }
-    
-                if(flashcard.lesson_type == "iframe_link"){
-                    addIframeLink(false,flashcard.id,flashcard.question,flashcard.options,flashcard.image)
-                }
-    
-            })
-            
-        })
-    }
 
 
 
 $(document).on("click",".remove_flashcard",function(e){
     var lesson_element_type = $(e.target).parent().parent().children().last().children().attr("name")
-    console.log(lesson_element_type)
+    //console.log(lesson_element_type)
     if(lesson_element_type.startsWith("speed_read")){
         quick_read_count--;
     }else if (lesson_element_type.startsWith("text")){
@@ -295,17 +312,18 @@ $(document).on("click",".remove_flashcard",function(e){
     }else if (lesson_element_type.startsWith("video")){
         video_file_count--
     }
-    console.log(lesson_element_type)
+    //console.log(lesson_element_type)
     $(e.target).parent().parent().remove()
 });
 
 
 
-/*
+
 $.get(API_SERVER+'/courses_api/lesson/all',function (response) {
     console.log(response.data);
 })
-*/
+
+
 $('#add').click(function (e) {
 
     if ($("#selectsegment").val() == 'speed_read')
@@ -337,6 +355,10 @@ $('#add').click(function (e) {
     }
 });
 
+
+
+
+/*
 $("#edit_form").submit((e) => {
     e.preventDefault()
     data_ = {
@@ -441,14 +463,5 @@ $("#edit_form").submit((e) => {
     console.log(data_)
 
 
-    $.ajax({
-        "url": API_SERVER+"/courses_api/lesson/update/"+lesson_id+"/",
-        'data': JSON.stringify(data_),
-        'type': 'POST',
-        'contentType': 'application/json',
-        'success': function (data){
-        alert("FlashCard Updated!")
-        location.reload()
-    }
-    })
 })
+*/
