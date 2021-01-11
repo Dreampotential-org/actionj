e148e4aba888a9e34fdd24473c5a1b278023eca3
Santosh <webdevelopersbc@gmail.com>
2021-01-11 20:08:06 +0530

    worked on login starter

---
+++ www/js/login_starter.js
@@ -0,0 +1,98 @@
+var SERVER = "https://sfapp-api.dreamstate-4-all.org/";
+//var SERVER = 'http://localhost:8001/'
+
+var passwordResetToken = getParam("token");
+var userToken = localStorage.getItem("user-token");
+
+console.log("MODE: PASSWORD_RESET, Token - " + passwordResetToken);
+
+function getParam(sParam) {
+  var sPageURL = window.location.search.substring(1);
+  var sURLVariables = sPageURL.split("&");
+  for (var i = 0; i < sURLVariables.length; i++) {
+    var sParameterName = sURLVariables[i].split("=");
+    if (sParameterName[0] == sParam) {
+      return sParameterName[1];
+    }
+  }
+}
+
+$(document).ready(function () {
+  let MODE = "WELCOME_PAGE";
+
+  if (passwordResetToken) {
+    MODE = "PASSWORD_RESET";
+  } else if (userToken) {
+    MODE = "HOME_PAGE";
+  }
+
+  $("#signUpForm").on("submit", function (ev) {
+    ev.preventDefault();
+
+    $.ajax({
+      url: SERVER + "s3_uploader/user/register",
+      type: "post",
+      data: $(this).serialize(),
+      success: function (response) {
+        // Whatever you want to do after the form is successfully submitted
+        console.log(response);
+        localStorage.setItem("user-token", response.token);
+
+        swal({
+          title: "Welcome " + response.user.name + "!",
+          text: "Your account is created.",
+          icon: "success",
+          buttons: false,
+          timer: 1000,
+        });
+
+        displayPage("dashboard");
+      },
+      error: function () {
+        swal({
+          title: "Error",
+          text: "",
+          icon: "error",
+        });
+      },
+    });
+  });
+
+  $("#loginForm").on("submit", function (ev) {
+    debugger;
+    ev.preventDefault();
+    swal({
+      title: "Signing In!",
+      icon: "success",
+      buttons: false,
+    });
+
+    $.ajax({
+      url: SERVER + "s3_uploader/user/login",
+      type: "post",
+      data: $(this).serialize(),
+      success: function (response) {
+        // Whatever you want to do after the form is successfully submitted
+        console.log(response);
+        localStorage.setItem("user-token", response.token);
+
+        swal({
+          title: "Welcome " + response.user.name + "!",
+          text: "You are logged in.",
+          icon: "success",
+          buttons: false,
+          timer: 1000,
+        });
+
+        displayPage("dashboard");
+      },
+      error: function () {
+        swal({
+          title: "Error",
+          text: "",
+          icon: "error",
+        });
+      },
+    });
+  });
+});
