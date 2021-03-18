0eb026de4515e694d0c97374cf7d82c37744a2dd
Irfan Ullah <irfanullahmarwat55@gmail.com>
2021-03-19 02:46:09 +0500

    design stadium listing page

--- www/BookStadium/js/list.js
+++ www/BookStadium/js/list.js
@@ -1,19 +1,14 @@
-var $stadiums = $('#stadiums') ;
+var $stadiums = $('#stadiums');
 
- $.ajax({
-                  url: 'https://api.dreampotential.org/bookingstadium/api/stadium/list',
-                  type: 'GET',
-                  success: function(data) {
-                  
-                     for (var i = 0; i < data.length; i++) {
-                      var stadium = data[i];
-                      $stadiums.append('<l>'+stadium.name+'</l><br>');
-                      $stadiums.append('<img src='+stadium.image+' witdh="400" height="300"></>');
-                      $stadiums.append('<p>Capacity:'+stadium.capacity+'</p>')
-                      $stadiums.append('<p>Country:'+stadium.country+'</p>')
-                      $stadiums.append('<p>City:'+stadium.city+'</p>')
-                      $stadiums.append('<p>Sport:'+stadium.sports+'</p></br>')
-                     }
-                     }
-                  })
+$.ajax({
+    url: 'https://api.dreampotential.org/bookingstadium/api/stadium/list',
+    type: 'GET',
+    success: function (data) {
+
+        for (var i = 0; i < data.length; i++) {
+            var stadium = data[i];
+            $stadiums.append('<tr><td>' + stadium.name + '</td><td><img src=' + stadium.image + ' class="img-sizing"></td><td>' + stadium.capacity + '</td><td>' + stadium.country + '</td><td>' + stadium.city + '</td><td>' + stadium.sports + '</td></tr>')
+        }
+    }
+})
       
