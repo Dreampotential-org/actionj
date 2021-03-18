0c5a717df20444821a131f1c8d8f9c88c21b5232
vusalbabayev <babayevvusal007@mail.com>
2021-03-18 17:04:35 +0400

    updated stadium list

--- www/BookStadium/js/list.js
+++ www/BookStadium/js/list.js
@@ -1,14 +1,19 @@
 var $stadiums = $('#stadiums') ;
 
  $.ajax({
-                  url: 'https://api.dreampotential.org/bookingstadium/api/stadium/list',
+                  url: 'https://sfapp-api.dreamstate-4-all.org/neighbormade/stadiums',
                   type: 'GET',
                   success: function(data) {
-
-                     $.each(data,function(i,stadium){
-                      $stadiums.append('<li>'+stadium.name+'</li>');
+                  
+                     for (var i = 0; i < data.stadiums.length; i++) {
+                      var stadium = data.stadiums[i];
+                      $stadiums.append('<l>'+stadium.name+'</l><br>');
                       $stadiums.append('<img src='+stadium.image+' witdh="400" height="300"></>');
-                      $stadiums.append('<p>capacity:'+stadium.capacity+'</p>')
-                     })
-                  }
-      });
+                      $stadiums.append('<p>Capacity:'+stadium.capacity+'</p>')
+                      $stadiums.append('<p>Country:'+stadium.country+'</p>')
+                      $stadiums.append('<p>City:'+stadium.city+'</p>')
+                      $stadiums.append('<p>Sport:'+stadium.sports+'</p></br>')
+                     }
+                     }
+                  })
+      
