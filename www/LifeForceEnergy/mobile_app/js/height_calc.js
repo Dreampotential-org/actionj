d7274ca7c21896967d590a29263cc1ea95f8f36a
Version Control <59991685+itzsammy23@users.noreply.github.com>
2021-05-05 22:14:03 +0100

    Page height setter update

--- www/LifeForceEnergy/mobile_app/js/height_calc.js
+++ www/LifeForceEnergy/mobile_app/js/height_calc.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    var content_height = $("#step__container").outerHeight();
+    var content_height = $(".calc__height").eq(0).outerHeight();
 
     $(".main").css({
         height: content_height + 250 + 'px'
