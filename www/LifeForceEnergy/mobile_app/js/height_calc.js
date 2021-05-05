96d3794117deb279b596bf736c0a1981fab3fe06
Version Control <59991685+itzsammy23@users.noreply.github.com>
2021-05-05 23:58:57 +0100

    Update height_calc.js

--- www/LifeForceEnergy/mobile_app/js/height_calc.js
+++ www/LifeForceEnergy/mobile_app/js/height_calc.js
@@ -2,7 +2,9 @@ $(document).ready(function () {
     var content_height = $(".calc__height").eq(0).outerHeight();
 
     $(".main").css({
-        height: content_height + 250 + 'px'
+        height: content_height + 200 + 'px'
     })
+
+    $(".bottom__link").removeClass("hidden__content");
 
 })
